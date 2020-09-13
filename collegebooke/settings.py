73ea3a27af4f9f131d3f8696f46db60402479f77
divC404 <diveshchoudhary20@gmail.com>
2020-09-13 23:23:35 +0530

    login validation and logout function of user

--- collegebooke/settings.py
+++ collegebooke/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'books.apps.BooksConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'books-home'
+LOGIN_URL = 'login'
